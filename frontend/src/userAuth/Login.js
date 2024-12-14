8093417fd094d6d7f00f4aa8ac11b0bcc9b1e384
Johnathan Aguilar <johnathan.aguilar.350@my.csun.edu>
2024-12-13 17:09:00 -0800

    Finished the login component

--- frontend/src/userAuth/Login.js
+++ frontend/src/userAuth/Login.js
@@ -1,11 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import IconButton from "@mui/material/IconButton";
+import InputAdornment from "@mui/material/InputAdornment";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
 import "./Login.css";
 
 function Login() {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleTogglePasswordVisibility = () => {
+    setShowPassword((prev) => !prev);
+  };
+
   return (
     <>
       <div className="login">
-        <img className="login-logo" src="/HalfLogo.png" alt="logo" />
+        <div className="login-logo">
+          <img className="login-logo-img" src="/HalfLogo.png" alt="logo" />
+        </div>
+        <div className="login-textfeild">
+          <Box
+            component="form"
+            sx={{
+              "& > :not(style)": { m: 1, width: "400px" },
+            }}
+            noValidate
+            autoComplete="off"
+          >
+            <TextField id="username" label="Username" variant="outlined" />
+          </Box>
+          <Box
+            component="form"
+            sx={{
+              "& > :not(style)": { m: 1, width: "400px" },
+            }}
+            noValidate
+            autoComplete="off"
+          >
+            <TextField
+              id="password"
+              label="Password"
+              variant="outlined"
+              type={showPassword ? "text" : "password"}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton
+                      onClick={handleTogglePasswordVisibility}
+                      edge="end"
+                    >
+                      {showPassword ? <Visibility /> : <VisibilityOff />}
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }}
+            />
+          </Box>
+        </div>
+        <button className="login-button">Login</button>
       </div>
     </>
   );
