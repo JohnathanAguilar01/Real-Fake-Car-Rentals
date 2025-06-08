5735df16cc52752a904c791d5d7f643bb163d6f6
Johnathan Aguilar <johnathan.aguilar.350@my.csun.edu>
2025-06-07 19:31:38 -0700

    updated env to work with jest test sweet and esm modules

--- seederentrypoint.sh
+++ seederentrypoint.sh
@@ -5,7 +5,7 @@ if [ ! -f /app/database/.seeds_run ]; then
   echo "Running knex seeds..."
   npx knex seed:run --knexfile /app/database/knexfile.js
   # Create a marker file to indicate that seeds have been executed
-  touch /app/database/.seeds_run
+  # touch /app/database/.seeds_run
 else
   echo "Seeds already run. Skipping seeding step."
 fi
