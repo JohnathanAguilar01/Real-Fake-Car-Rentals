4f03ae8ad134b649c5eb2ec92ba40b5945bdad8f
Johnathan Aguilar <johnathan.aguilar.350@my.csun.edu>
2025-07-15 18:21:50 -0700

    fixed app.test.ts to work with vitest instead of jest

--- backend/tests/app.test.ts
+++ backend/tests/app.test.ts
@@ -1,8 +1,9 @@
 import request from "supertest";
-import app from "../app";
+import { describe, it, expect } from "vitest";
+import app from "../src/app.ts";
 
 describe("GET app.com/", () => {
-  test('should return a message response with "Backend is running!"', async () => {
+  it('should return a message response with "Backend is running!"', async () => {
     const response = await request(app).get("/");
 
     expect(response.statusCode).toBe(200);
