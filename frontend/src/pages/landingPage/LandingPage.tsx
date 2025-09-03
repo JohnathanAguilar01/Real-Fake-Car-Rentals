4b8e4bbe5a557032bb782b8444d61ffac3871236
Johnathan Aguilar <johnathan.aguilar.350@my.csun.edu>
2025-09-02 23:51:14 -0700

    got more of the landing page done

--- frontend/src/pages/landingPage/LandingPage.tsx
+++ frontend/src/pages/landingPage/LandingPage.tsx
@@ -7,10 +7,22 @@ export default function LandingPage() {
         <h1 className="text-figmaprimary font-['Archivo_Black'] text-3xl">
           Real Fake Car Rentals
         </h1>
-        <Button className="bg-figmaprimary w-24 rounded-2xl text-center text-md font-['Archivo_Black']">
+        <Button className="bg-figmaprimary w-32 h-10 rounded-2xl text-center text-md font-['Archivo_Black']">
           Sign Up
         </Button>
       </nav>
+      <div className="flex bg-[url('/car_background.jpg')] w-screen h-156 bg-cover bg-center justify-center items-center pr-128 shadow-lg">
+        <div className="flex flex-col w-256 h-64 bg-figmabackground rounded-lg px-16 justify-center">
+          <h1 className="text-3xl font-['Archivo_Black']">
+            Looking to save more
+          </h1>
+          <h1 className="text-3xl font-['Archivo_Black']">
+            on your car rental?
+          </h1>
+          <div className="h-1 w-32 bg-figmaprimary my-2" />
+          <p className="font-['Archivo']">Get your best car rental today!</p>
+        </div>
+      </div>
     </>
   );
 }
