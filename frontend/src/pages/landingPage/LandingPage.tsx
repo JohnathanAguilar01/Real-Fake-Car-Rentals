42bb29ee96031e7f63b734da42bed2c93de18228
Johnathan Aguilar <johnathan.aguilar.350@my.csun.edu>
2025-09-03 11:09:09 -0700

    made the main search part of the landing page

--- frontend/src/pages/landingPage/LandingPage.tsx
+++ frontend/src/pages/landingPage/LandingPage.tsx
@@ -1,4 +1,14 @@
 import { Button } from "@/components/ui/button";
+import DateRangePicker from "../../components/date-picker/date-range-picker.tsx";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 export default function LandingPage() {
   return (
@@ -12,7 +22,7 @@ export default function LandingPage() {
         </Button>
       </nav>
       <div className="flex bg-[url('/car_background.jpg')] w-screen h-156 bg-cover bg-center justify-center items-center pr-128 shadow-lg">
-        <div className="flex flex-col w-256 h-64 bg-figmabackground rounded-lg px-16 justify-center">
+        <div className="flex flex-col w-fit h-64 bg-figmabackground rounded-lg px-16 justify-center">
           <h1 className="text-3xl font-['Archivo_Black']">
             Looking to save more
           </h1>
@@ -21,6 +31,26 @@ export default function LandingPage() {
           </h1>
           <div className="h-1 w-32 bg-figmaprimary my-2" />
           <p className="font-['Archivo']">Get your best car rental today!</p>
+          <div className="flex gap-2 bg-figmavarient w-fit p-3 rounded-lg my-2">
+            <DateRangePicker />
+            <Select>
+              <SelectTrigger className="w-[128px] bg-figmabackground">
+                <SelectValue placeholder="Type" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectLabel>Type</SelectLabel>
+                  <SelectItem value="sedan">Sedan</SelectItem>
+                  <SelectItem value="truck">Truck</SelectItem>
+                  <SelectItem value="coupe">Coupe</SelectItem>
+                  <SelectItem value="suv">SUV</SelectItem>
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+            <Button className="w-32 bg-figmaprimary text-2xl font-['Archivo_Black']">
+              Find
+            </Button>
+          </div>
         </div>
       </div>
     </>
