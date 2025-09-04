00972499a77d2a98dbe9df4b43023dfd4148a337
Johnathan Aguilar <johnathan.aguilar.350@my.csun.edu>
2025-09-04 16:35:56 -0700

    split the landing page up into diffrent components

--- frontend/src/components/mainComponents/NavBar.tsx
+++ frontend/src/components/mainComponents/NavBar.tsx
@@ -2,8 +2,13 @@ import { Button } from "../ui/button";
 
 export default function NavBar() {
   return (
-    <>
-      <Button />
-    </>
+    <nav className="w-screen h-16 shadow-lg bg-figmabackground flex justify-between items-center p-4">
+      <h1 className="text-figmaprimary font-['Archivo_Black'] text-3xl">
+        Real Fake Car Rentals
+      </h1>
+      <Button className="bg-figmaprimary w-32 h-10 rounded-2xl text-center text-md font-['Archivo_Black']">
+        Sign Up
+      </Button>
+    </nav>
   );
 }
--- frontend/src/pages/landingPage/LandingPage.tsx
+++ frontend/src/pages/landingPage/LandingPage.tsx
@@ -1,58 +1,11 @@
-import { Button } from "@/components/ui/button";
-import DateRangePicker from "../../components/date-picker/date-range-picker.tsx";
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectLabel,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
+import NavBar from "@/components/mainComponents/NavBar";
+import LandingSearch from "./LandingSearch";
 
 export default function LandingPage() {
   return (
     <>
-      <nav className="w-screen h-16 shadow-lg bg-figmabackground flex justify-between items-center p-4">
-        <h1 className="text-figmaprimary font-['Archivo_Black'] text-3xl">
-          Real Fake Car Rentals
-        </h1>
-        <Button className="bg-figmaprimary w-32 h-10 rounded-2xl text-center text-md font-['Archivo_Black']">
-          Sign Up
-        </Button>
-      </nav>
-      <div className="flex bg-[url('/car_background.jpg')] w-screen h-156 bg-cover bg-center justify-center items-center pr-128 shadow-lg">
-        <div className="flex flex-col w-fit h-64 bg-figmabackground rounded-lg px-16 justify-center">
-          <h1 className="text-3xl font-['Archivo_Black']">
-            Looking to save more
-          </h1>
-          <h1 className="text-3xl font-['Archivo_Black']">
-            on your car rental?
-          </h1>
-          <div className="h-1 w-32 bg-figmaprimary my-2" />
-          <p className="font-['Archivo']">Get your best car rental today!</p>
-          <div className="flex gap-2 bg-figmavarient w-fit p-3 rounded-lg my-2">
-            <DateRangePicker />
-            <Select>
-              <SelectTrigger className="w-[128px] bg-figmabackground">
-                <SelectValue placeholder="Type" />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectGroup>
-                  <SelectLabel>Type</SelectLabel>
-                  <SelectItem value="sedan">Sedan</SelectItem>
-                  <SelectItem value="truck">Truck</SelectItem>
-                  <SelectItem value="coupe">Coupe</SelectItem>
-                  <SelectItem value="suv">SUV</SelectItem>
-                </SelectGroup>
-              </SelectContent>
-            </Select>
-            <Button className="w-32 bg-figmaprimary text-2xl font-['Archivo_Black']">
-              Find
-            </Button>
-          </div>
-        </div>
-      </div>
+      <NavBar />
+      <LandingSearch />
     </>
   );
 }
