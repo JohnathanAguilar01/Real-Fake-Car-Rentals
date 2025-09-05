e8f76eb28c6b4175f1a3318a726d30d1e24802e3
Johnathan Aguilar <johnathan.aguilar.350@my.csun.edu>
2025-09-04 17:04:35 -0700

    added an about page

---
+++ frontend/src/pages/landingPage/LandingAbout.tsx
@@ -0,0 +1,15 @@
+export default function LandingAbout() {
+  return (
+    <div className="w-screen flex flex-col items-center justify-start px-12 py-24">
+      <div className="w-24 h-1 bg-figmaprimary mb-2" />
+      <h2 className="text-3xl font-['Archivo_Black'] mb-2">About Us</h2>
+      <p className="text-3xl font-['Archivo'] mx-32 text-center">
+        At Real Fake Car Rentals, we make renting a car simple, affordable, and
+        hassle-free. From compact cars to spacious SUVs and luxury models, our
+        fleet is built to match every journey. With easy online booking,
+        transparent pricing, and a commitment to excellent service, we ensure
+        every customer enjoys a smooth and reliable rental experience.
+      </p>
+    </div>
+  );
+}
--- frontend/src/pages/landingPage/LandingPage.tsx
+++ frontend/src/pages/landingPage/LandingPage.tsx
@@ -1,11 +1,13 @@
 import NavBar from "@/components/mainComponents/NavBar";
 import LandingSearch from "./LandingSearch";
+import LandingAbout from "./LandingAbout";
 
 export default function LandingPage() {
   return (
     <>
       <NavBar />
       <LandingSearch />
+      <LandingAbout />
     </>
   );
 }
